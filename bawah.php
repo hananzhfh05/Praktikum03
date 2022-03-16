638ec2dd4adf2d2377b57a3aad04b99a5a8a7350
hananzhfh05 <100538468+hananzhfh05@users.noreply.github.com>
2022-03-16 17:43:24 +0700

    Add files via upload

---
+++ bawah.php
@@ -0,0 +1,7 @@
+<div style="background-color: #0d6efd; height: 60px;">
+<footer class="pt-3">
+    <p class="text-center">Created by <a class="text-dark" href="http://nurulfikri.ac.id">Pusinfo NF &copy;2017</a></p>
+</footer>
+
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<nav class="sb-topnav navbar navbar-expand" style="background-color:#1E90FF;">
+<?php
+    include_once 'atas.php';
+?>
+</nav>
+<h1 class="text-center mt-4">Welcome Home !!! </h1>
+<P class="mt-4 container fluid" style="text-indent: 35px; text-align:justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam eius veniam harum debitis cum suscipit asperiores, recusandae commodi odit adipisci ipsa? Illum laboriosam ex omnis, necessitatibus inventore nemo ea molestiae!
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid itaque porro expedita ipsa doloribus, repudiandae veritatis cupiditate commodi quidem est vero repellendus adipisci, nam assumenda atque aliquam, id accusamus. Beatae.
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur eligendi autem non magni recusandae, dolor distinctio iusto adipisci ipsam, totam repudiandae ipsum vero voluptatum in dolorem eveniet illo. Aliquam, dolor?
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate quas quibusdam, ab quaerat veniam a illum consequuntur nulla neque esse ullam asperiores aliquam deserunt doloribus quos nihil perferendis. Quibusdam, sed.
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta aut alias harum deleniti necessitatibus ducimus voluptates ex, corrupti doloremque omnis laborum sunt similique consectetur modi iusto excepturi impedit recusandae eius.
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus debitis fugiat modi molestias accusantium, maiores, explicabo a ratione quos minima ipsam delectus incidunt quibusdam odio rem, tempore nam natus nulla.
+</p>
+
+<P class="mt-4 container fluid" style="text-indent: 35px; text-align:justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam eius veniam harum debitis cum suscipit asperiores, recusandae commodi odit adipisci ipsa? Illum laboriosam ex omnis, necessitatibus inventore nemo ea molestiae!
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid itaque porro expedita ipsa doloribus, repudiandae veritatis cupiditate commodi quidem est vero repellendus adipisci, nam assumenda atque aliquam, id accusamus. Beatae.
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur eligendi autem non magni recusandae, dolor distinctio iusto adipisci ipsam, totam repudiandae ipsum vero voluptatum in dolorem eveniet illo. Aliquam, dolor?
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate quas quibusdam, ab quaerat veniam a illum consequuntur nulla neque esse ullam asperiores aliquam deserunt doloribus quos nihil perferendis. Quibusdam, sed.
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta aut alias harum deleniti necessitatibus ducimus voluptates ex, corrupti doloremque omnis laborum sunt similique consectetur modi iusto excepturi impedit recusandae eius.
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus debitis fugiat modi molestias accusantium, maiores, explicabo a ratione quos minima ipsam delectus incidunt quibusdam odio rem, tempore nam natus nulla.
+</p>
+<br>
+<?php
+    require_once 'bawah.php';
+?>
+</body>
+</html>
